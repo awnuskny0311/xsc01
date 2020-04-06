33475104dfac3fc640d8d9bbf02656fae8233dab
awnuskny0311 <39233132+awnuskny0311@users.noreply.github.com>
2020-04-06 14:23:10 +0800

    Update RedisUtil.java

--- src/main/java/RedisUtil.java
+++ src/main/java/RedisUtil.java
@@ -2,4 +2,9 @@ public class RedisUtil {
     public static void main(String[] args) {
         System.out.println("bbbbbbbbbbbbbbbbbb");
     }
+    public void priex(){
+        
+        System.out.println("111111");
+        
+    }
 }
